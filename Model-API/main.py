ea0ad84f57602dfe442697e2ca29c173ea592e0e
Usman Malik <usmanmalik740@gmail.com>
2023-04-13 22:56:42 +0500

    Model API - Usman

---
+++ Model-API/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from GTRS2 import TWC_GTRS_MODEL
+from flask_cors import CORS
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['POST'])
+def search():
+
+    data = request.get_json()
+    my_df =  pd.json_normalize(data)
+    model = TWC_GTRS_MODEL()
+    predictions = model.predict(my_df)
+    results = predictions
+    return jsonify({'results': results})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
